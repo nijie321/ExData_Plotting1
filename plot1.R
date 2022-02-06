f4a489d37f7789213f71bd476553ca08c95d52fe
Jie Ni <nijie321@outlook.com>
2022-02-06 10:47:18 -0600

    add plot 1 code and figure

---
+++ plot1.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(readr)
+
+# Loading the data
+# the data file must be contained in a directory named 'data' under the current working directory
+df <- read_delim("./data/household_power_consumption.txt",
+                 col_types = "ccddddddn",na = c("?",""))
+
+## select subset of data in 2007-02-01 and 2007-02-02
+df <- filter(df, Date=="1/2/2007"|Date=="2/2/2007")
+
+# create new datetime variable using Date and Time columns
+df$datetime <- strptime(paste(df$Date, df$Time),
+                        format = "%d/%m/%Y %H:%M:%S")
+
+
+png(filename = "plot1.png", width = 480, height = 480)
+with(df, hist(Global_active_power, col="red",
+              xlab = "Global Active Power (kilowatts)",
+              main="Global Active Power"))
+dev.off()
